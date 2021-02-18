e2c4a5fe239474cdcb32bc966d8039ea32863d48
Ryan Kelley <kelleyr@duvalschools.org>
2021-02-18 12:32:17 -0500

    v0.1
    Created d4() function to roll a d4 a certain number of times.

--- dice_roll.py
+++ dice_roll.py
@@ -1,2 +1,13 @@
-# Dice Roll Library, Ryan Kelley, February 18, 2021, 12:12PM, v0.0 
+# Dice Roll Library, Ryan Kelley, February 18, 2021, 12:31PM, v0.1 
+
+# d4 Simulator 
+def roll_d4(num_roll): # num_roll is an ARGUMENT. 
+    import random 
+    
+    rolls = 0 
+    while rolls <= num_roll: 
+        result = random.randint(1,4)
+        print(f"You rolled a {result} on the d4!\n")
+        rolls += 1 
+
 
