d710a67e2318c3c1c2581c52aa348b2c5f4df001
Ryan Kelley <kelleyr@duvalschools.org>
2021-02-18 12:48:33 -0500

    v0.3
    Finished d4() function, now includes a total of all the rolls.

--- dice_roll.py
+++ dice_roll.py
@@ -1,13 +1,20 @@
-# Dice Roll Library, Ryan Kelley, February 18, 2021, 12:31PM, v0.1 
+# Dice Roll Library, Ryan Kelley, February 18, 2021, 12:47PM, v0.3 
 
 # d4 Simulator 
 def roll_d4(num_roll): # num_roll is an ARGUMENT. 
     import random 
-    
+    import time 
+
     rolls = 0 
-    while rolls <= num_roll: 
+    sum = 0 
+    while rolls < num_roll: 
         result = random.randint(1,4)
         print(f"You rolled a {result} on the d4!\n")
-        rolls += 1 
+        rolls += 1
+        time.sleep(1)  
+        sum += result 
+    print(f"The total roll was {sum}.\n")
+
+roll_d4(5)
 
 
